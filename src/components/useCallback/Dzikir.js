133d7ef6fd84459a67b7911136c8956cd90ce4b2
abdymalikmulky <abdymalikmulky@gmail.com>
2020-01-16 07:32:17 +0700

    useHook

--- src/components/useCallback/Dzikir.js
+++ src/components/useCallback/Dzikir.js
@@ -1,9 +1,12 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import Counter from "./Counter";
 import ButtonIncrement from "./ButtonIncrement";
 import Title from "./Title";
 
 function Dzikir() {
+  const [countIstigfar, setCountIstigfar] = useState(0);
+  const [countLailaha, setCountLailaha] = useState(0);
+
   const [countSub, setCountSub] = useState(0);
   const [countAlh, setCountAlh] = useState(0);
   const [countAll, setCountAll] = useState(0);
@@ -17,6 +20,19 @@ function Dzikir() {
   const incrAll = useCallback(() => {
     setCountAll(countAll + 1);
   }, [countAll]);
+
+  const incrIstigfar = () => {
+    setCountIstigfar(countIstigfar + 1);
+  };
+
+  const incrLailaha = () => {
+    setCountLailaha(countLailaha + 1);
+  };
+
+  const isEven = useMemo(() => {
+    console.log("countIstigfar", countIstigfar);
+    return countIstigfar % 2 === 0;
+  }, [countIstigfar]);
 
   return (
     <div>
@@ -33,6 +49,29 @@ function Dzikir() {
         <div className="col m4">
           <Counter count={countAll}>ALLAHUAKBAR</Counter>
           <ButtonIncrement onClick={incrAll}>Dzikir ALL</ButtonIncrement>
+        </div>
+        <div className="row center">
+          <div className="col m6">
+            <h4>
+              {countIstigfar} - {isEven ? "GENAP" : "GANJIL"}
+            </h4>
+            <button
+              onClick={incrIstigfar}
+              className="waves-effect waves-light btn btn-block"
+            >
+              Astagfirullah
+            </button>
+          </div>
+
+          <div className="col m6">
+            <h4>{countLailaha}</h4>
+            <button
+              onClick={incrLailaha}
+              className="waves-effect waves-light btn btn-block"
+            >
+              Lailahailallah
+            </button>
+          </div>
         </div>
       </div>
     </div>
